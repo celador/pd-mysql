ce44b861d3cd8801af100c4727259e396fa7bc30
John Nelson <john.aaron.nelson@gmail.com>
2024-04-21 12:05:39 -0400

    hmm

--- src/my.grammar.ts
+++ src/my.grammar.ts
@@ -2,16 +2,16 @@
 import {LRParser} from "@lezer/lr"
 export const parser = LRParser.deserialize({
   version: 14,
-  states: "#SQQOPOOOVOPO'#C_OOOO'#C^'#C^OOOO'#Cd'#CdQQOPOOOOOO'#Ce'#CeO_OPO,58yOgOPO,58yOOOO-E6b-E6bOOOO-E6c-E6cOoOPO1G.eOoOPO1G.eOOOO1G.e1G.eOwOPO7+$POOOO7+$P7+$POOOO<<Gk<<Gk",
-  stateData: "!P~OSPO~OTTOUVO~OTTOUYO~OTTOV[O~OTTOV^O~OTTOV_O~O",
-  goto: "wYPPZ_PPPPciTROSTQOSQSORWSQUPUXUZ]QZVR]Y",
-  nodeNames: "⚠ Program Statement TemplateExpression OpenCurly Whitespace VariablePath CloseCurly",
-  maxTerm: 10,
+  states: "#`QQOPOOOVOPO'#CeOOOO'#Ce'#CeQQOPOOObOQO'#C`OjOPO,58yOOOO-E6c-E6cOOOO'#Cf'#CfOoOQO,58zOwOQO,58zOOOO1G.e1G.eOOOO-E6d-E6dO!POQO1G.fO!POQO1G.fOOOO1G.f1G.fO!XOQO7+$QOOOO7+$Q7+$QOOOO<<Gl<<Gl",
+  stateData: "!d~OQPO~OTSOQXXZXX~OUVOVXO~OQYO~OUVOV[O~OUVOW^O~OUVOW`O~OUVOWaO~OTW~",
+  goto: "wZPPP[`PPPPciTQORRTPQRORURQWSUZW]_Q]XR_[",
+  nodeNames: "⚠ Program Text Statement TemplateExpression OpenCurly Whitespace VariablePath CloseCurly",
+  maxTerm: 11,
   skippedNodes: [0],
   repeatNodeCount: 2,
-  tokenData: "!{~R^X^}pq}!c!}!S#T#o!S#o#p!e#q#r!p#y#z}$f$g}#BY#BZ}$IS$I_}$I|$JO}$JT$JU}$KV$KW}&FU&FV}~!SOT~~!XSU~!O!P!S!Q![!S!c!}!S#T#o!S~!hP#o#p!k~!pOS~~!sP#q#r!v~!{OV~",
-  tokenizers: [0],
+  tokenData: "$o~RnOX#PX^#U^p#Ppq#Uq!c#P!c!}#]!}#T#P#T#o#]#o#p$R#p#q#P#q#r$^#r#y#P#y#z#U#z$f#P$f$g#U$g#BY#P#BY#BZ#U#BZ$IS#P$IS$I_#U$I_$I|#P$I|$JO#U$JO$JT#P$JT$JU#U$JU$KV#P$KV$KW#U$KW&FU#P&FU&FV#U&FV;'S#P;'S;=`$i<%lO#PP#UOQPR#]OQPUQR#dSQPVQ!O!P#p!Q![#p!c!}#p#T#o#pQ#uSVQ!O!P#p!Q![#p!c!}#p#T#o#p~$UP#o#p$X~$^OT~~$aP#q#r$d~$iOW~P$lP;=`<%l#P",
+  tokenizers: [0, 1],
   topRules: {"Program":[0,1]},
-  tokenPrec: 0
+  tokenPrec: 63
 })
 
