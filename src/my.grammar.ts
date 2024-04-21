64372d366747fe526c038f10be90f23aa4b83930
John Nelson <john.aaron.nelson@gmail.com>
2024-04-21 12:08:44 -0400

    next grammar

--- src/my.grammar.ts
+++ src/my.grammar.ts
@@ -3,7 +3,7 @@ import {LRParser} from "@lezer/lr"
 export const parser = LRParser.deserialize({
   version: 14,
   states: "#`QQOPOOOVOPO'#CeOOOO'#Ce'#CeQQOPOOObOQO'#C`OjOPO,58yOOOO-E6c-E6cOOOO'#Cf'#CfOoOQO,58zOwOQO,58zOOOO1G.e1G.eOOOO-E6d-E6dO!POQO1G.fO!POQO1G.fOOOO1G.f1G.fO!XOQO7+$QOOOO7+$Q7+$QOOOO<<Gl<<Gl",
-  stateData: "!d~OQPO~OTSOQXXZXX~OUVOVXO~OQYO~OUVOV[O~OUVOW^O~OUVOW`O~OUVOWaO~OTW~",
+  stateData: "!a~OQPO~OTSOQXXZXX~OUVOVXO~OQYO~OUVOV[O~OUVOW^O~OUVOW`O~OUVOWaO~O",
   goto: "wZPPP[`PPPPciTQORRTPQRORURQWSUZW]_Q]XR_[",
   nodeNames: "⚠ Program Text Statement TemplateExpression OpenCurly Whitespace VariablePath CloseCurly",
   maxTerm: 11,
@@ -12,6 +12,6 @@ export const parser = LRParser.deserialize({
   tokenData: "$o~RnOX#PX^#U^p#Ppq#Uq!c#P!c!}#]!}#T#P#T#o#]#o#p$R#p#q#P#q#r$^#r#y#P#y#z#U#z$f#P$f$g#U$g#BY#P#BY#BZ#U#BZ$IS#P$IS$I_#U$I_$I|#P$I|$JO#U$JO$JT#P$JT$JU#U$JU$KV#P$KV$KW#U$KW&FU#P&FU&FV#U&FV;'S#P;'S;=`$i<%lO#PP#UOQPR#]OQPUQR#dSQPVQ!O!P#p!Q![#p!c!}#p#T#o#pQ#uSVQ!O!P#p!Q![#p!c!}#p#T#o#p~$UP#o#p$X~$^OT~~$aP#q#r$d~$iOW~P$lP;=`<%l#P",
   tokenizers: [0, 1],
   topRules: {"Program":[0,1]},
-  tokenPrec: 63
+  tokenPrec: 0
 })
 
