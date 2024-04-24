f9eec18f26e46bd66911ee7cec68515d16d47a1c
John Nelson <john.aaron.nelson@gmail.com>
2024-04-24 09:25:30 -0400

    fixup

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
-import typescript from "rollup-plugin-ts";
-import { lezer } from "@lezer/generator/rollup";
+import typescript from 'rollup-plugin-ts'
+import { lezer } from '@lezer/generator/rollup'
 
 export default {
-  input: "src/index.ts",
-  external: (id) => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  input: 'src/index.ts',
+  external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
   output: [
-    { dir: "./dist", format: "es" },
+    { dir: './dist', format: 'es' }
   ],
-  plugins: [lezer(), typescript()],
-};
+  plugins: [lezer(), typescript()]
+}
